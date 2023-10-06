2c14df798c6d5ad90565a33fbc2f61da56e83fe0
Lyndon <lyndon@onthelink.net>
2023-10-06 22:08:55 +1300

    Fixed up logic, should work now

--- hook-lib.sh
+++ hook-lib.sh
@@ -7,7 +7,7 @@ source ${BASE_DIR}/defaults.config
 
 function log {
   DATESTRING=$(date -u '+%F %X')
-  echo -e "$DATESTRING | $1"
+  echo -e "$DATESTRING | $1" > /dev/stderr
 }
 
 # Catch errors
@@ -17,3 +17,16 @@ function log {
 # Make sure we got the LE env vars
 [ -z "$CERTBOT_DOMAIN" ] && log "ERROR: CERTBOT_DOMAIN is not defined!" && exit 1
 [ -z "$CERTBOT_VALIDATION" ] && log "ERROR: CERTBOT_DOMAIN is not defined!" && exit 1
+
+# Check to see if there is an existing acme record
+function check_for_acme_record {
+  log "Checking for ACME record '_acme-challenge.${CERTBOT_DOMAIN}'"
+  ZONE_RECORD="$(echo "$DNS_ZONE" | head -1 | jq ".return[] | select(.type==\"TXT\" and .name==\"_acme-challenge.${CERTBOT_DOMAIN}\")")"
+  if [ ! -z "$ZONE_RECORD" ]; then
+    log "Record '_acme-challenge.${CERTBOT_DOMAIN}' found: $ZONE_RECORD"
+    return 0
+  else
+    log "Record '_acme-challenge.${CERTBOT_DOMAIN}' not found"
+    return 1
+  fi
+}
--- sh-auth-hook.sh
+++ sh-auth-hook.sh
@@ -7,22 +7,34 @@ DNS_ZONE="$(curl -s -w "\n%{http_code}" ${API_ENDPOINT}/list_records.json?apikey
 [ "$(echo "$DNS_ZONE" | tail -1)" != "200" ] && log "ERROR: Did not get a 200 when querrying DNS zone, API return: 
 $DNS_ZONE" && exit 1
 
-# TODO test this
-# Check to see if there is an existing acme record
-#if [ ! -z "$(echo "$DNS_ZONE" | head -1 | jq ".return[] | select(.type==\"TXT\" and .name==\"_acme-challenge.${CERBOT_DOMAIN}\")")" ]; then
-#	# Uh oh, we need to wait around for a while
-#
-#	log "WARNING existing record for '_acme-challenge.${CERBOT_DOMAIN}' found, deleting an sleeping for 60 seconds..."
-#	#sleep 60
-#	#TODO add record delete
-#fi
+# If there is an existing record we need to delete it first
+if check_for_acme_record; then
+	log "WARNING existing record for '_acme-challenge.${CERTBOT_DOMAIN}' found"
+
+	ACME_RECORD_ID="$(echo "$ZONE_RECORD" | jq -r ".id")"
+	DELETE_RECORD="$(curl -s -w "\n%{http_code}" ${API_ENDPOINT}/delete_record.json?apikey=${API_KEY} --data "client_id=${CLIENT_ID}&domain=${CERTBOT_DOMAIN}&record_id=${ACME_RECORD_ID}")"
+[ "$(echo "$DELETE_RECORD" | tail -1)" != "200" ] && log "ERROR: Did not get a 200 when adding deleting record, API return: 
+$ADD_RECORD" && exit 1
+	log "Deleted record '_acme-challenge.${CERTBOT_DOMAIN}', sleeping for 60s..."
+	sleep 60
+
+fi
 
 # Okay lets add the record
 ADD_RECORD="$(curl -s -w "\n%{http_code}" ${API_ENDPOINT}/add_record.json?apikey=${API_KEY} --data "client_id=${CLIENT_ID}&domain=${CERTBOT_DOMAIN}&type=TXT&name=_acme-challenge.${CERTBOT_DOMAIN}&content=${CERTBOT_VALIDATION}")"
 [ "$(echo "$ADD_RECORD" | tail -1)" != "200" ] && log "ERROR: Did not get a 200 when adding validation record, API return: 
 $ADD_RECORD" && exit 1
 
-# Give it a second to flow through the SiteHost scheduler
-# TODO could poll for it here
+# Wait for it to flow through the SiteHost scheduler
+log "Waiting for record to exist in zone..."
+while true; do
+	check_for_acme_record && break
+	log "Checking..."
+	sleep 5
+done
+
+log "Digging and waiting 5 seconds for propigation."
+dig TXT _acme-challenge.${CERTBOT_DOMAIN} > /dev/stderr
 sleep 5
 
+log "Ready to validate."
